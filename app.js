a07e450ed48d5571fadaf9a8d628cdcd8ab90ffc
Zaldana <77674298+Zaldana@users.noreply.github.com>
2021-09-18 00:35:34 -0400

    app fs setup

--- app.js
+++ app.js
@@ -7,4 +7,13 @@ const teamRouter = require("./routes/teamRouter");
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
+app.use(logger("combined"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/team", teamRouter);
+
+app.listen(PORT, function () {
+    console.log(`Server is now running on PORT: ${PORT}`);
+});
