4b0c629200ad679150045d3cd1b609f65e0991e5
Zaldana <77674298+Zaldana@users.noreply.github.com>
2021-09-19 11:18:37 -0400

    working CRUD

--- routes/animalRouter.js
+++ routes/animalRouter.js
@@ -104,6 +104,94 @@ router.post("/", function (req, res) {
     }
 });
 
+router.put("/update-by-name/:animalName", function (req, res) {
 
+    let foundAnimal = null;
+
+    animalArray.forEach((item) => {
+        
+        if (item.animalName === req.params.animalName) {
+        
+            item.animalName = req.body.newAnimalname;
+            foundAnimal = true;
+        }
+    });
+
+    if (!foundAnimal) {
+        
+        res.send("Please check your spelling! Animal does not exists");
+    
+    } else {
+    
+        res.json({ animalArray });
+    }
+});
+
+router.put("/update-by-id/:id", function (req, res) {
+
+    let foundAnimal = null;
+
+    animalArray.forEach((item) => {
+
+        if (item.id === +req.params.id) {
+
+            item.animalName = req.body.newAnimalname;
+            foundAnimal = true;
+        }
+    });
+
+    if (!foundAnimal) {
+
+        res.send("ID does not exists");
+
+    } else {
+
+        res.json({ animalArray });
+    }
+});
+
+router.delete("/delete-by-id/:id", function (req, res) {
+   
+    let foundAnimal = null;
+
+    animalArray.forEach((item, index) => {
+        
+        if (item.id === +req.params.id) {
+            foundAnimal = item;
+            animalArray.splice(index, 1);
+        }
+    });
+
+    if (!foundAnimal) {
+
+        res.send("ID does not exists");
+    
+    } else {
+    
+        res.json({ animalArray, foundAnimal });
+    
+    }
+
+});
+
+router.delete("/delete-by-name/:animalName", function (req, res) {
+    
+    let foundAnimal = null;
+    
+    animalArray.forEach((item, index) => {
+        
+        if (item.animalName === req.params.animalName) {
+    
+            foundAnimal = item;
+            animalArray.splice(index, 1);
+        }
+    });
+
+    if (!foundAnimal) {
+        res.send("Pleasea check your spelling! Team does not exists");
+    } else {
+        res.json({ animalArray, foundAnimal });
+    }
+});
 
 module.exports = router;
