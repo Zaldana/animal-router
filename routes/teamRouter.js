182a76761e7437d721c79dc834f6429a4b6b884d
Zaldana <77674298+Zaldana@users.noreply.github.com>
2021-09-18 21:15:33 -0400

    routing of array

--- routes/teamRouter.js
+++ routes/teamRouter.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+let animalArray = [
+    { id: 1, animalName: "dog" },
+    { id: 2, animalName: "cat" },
+    { id: 3, animalName: "hamster" },
+];
+
 module.exports = router;
